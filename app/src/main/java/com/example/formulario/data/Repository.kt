789ee106c3c3b932deab4a14d56879609e423e5e
ines(2dam) <imartinezrodriguez@educa.madrid.org>
2023-10-02 08:51:40 +0200

    02/10/2023 08:51

---
+++ app/src/main/java/com/example/formulario/data/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.formulario.data
+
+class Repository {
+}
---
+++ app/src/main/java/com/example/formulario/domain/modelo/Persona.kt
@@ -0,0 +1,10 @@
+package com.example.formulario.domain.modelo
+
+class Persona {
+
+    data class Persona(val nombre:String?=null,
+                       val apellidos:String?=null,
+                       val email:String?=null,
+                       val edad:Int?=null)
+
+}
