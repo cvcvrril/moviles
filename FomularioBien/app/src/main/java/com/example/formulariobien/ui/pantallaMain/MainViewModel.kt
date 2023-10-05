f407d46e53d25c5b0971144d6a7e772e9a2cbba7
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-05 12:12:58 +0200

    05/10/2023 12:12

--- FomularioBien/app/src/main/java/com/example/formulariobien/ui/pantallaMain/MainViewModel.kt
+++ FomularioBien/app/src/main/java/com/example/formulariobien/ui/pantallaMain/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.formulariobien.ui.pantallaMain
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.formulariobien.domain.modelo.Pelicula
 import com.example.formulariobien.domain.usecases.personas.AddPeliculasUseCase
 import com.example.formulariobien.utils.StringProvider
 
@@ -10,7 +11,19 @@ class MainViewModel(
     private val stringProvider: StringProvider,
     private val addPeliculasUseCase: AddPeliculasUseCase
 ) : ViewModel(){
+
     private val _uiState = MutableLiveData<MainState>()
     val uiState: LiveData<MainState> get() = _uiState
+
+    fun addPelicula(pelicula: Pelicula){
+        if (!addPeliculasUseCase(pelicula)){
+            _uiState.value = MainState(
+                pelicula = _uiState.value.let { pelicula },
+                error = "name",
+            )
+            _uiState.value = _uiState
+                .value?.copy(error = Constantes.ERROR)
+        }
+    }
 
 }
