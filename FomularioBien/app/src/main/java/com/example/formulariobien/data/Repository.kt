6db6dd4e0d728aab1a3f80dc48cace5b57d77d25
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-05 14:14:15 +0200

    05/10/2023 14:14

--- FomularioBien/app/src/main/java/com/example/formulariobien/data/Repository.kt
+++ FomularioBien/app/src/main/java/com/example/formulariobien/data/Repository.kt
@@ -8,6 +8,12 @@ object Repository {
 
     init {
         peliculas.add(Pelicula("Litte Miss Sunshine"))
+        peliculas.add(Pelicula("In The Mood For Love"))
+        peliculas.add(Pelicula("Easy A"))
+        peliculas.add(Pelicula("Nope"))
+        peliculas.add(Pelicula("The Silence Of The Lambs"))
+        peliculas.add(Pelicula("The cook, el ladrón, his wife and her lover"))
+        peliculas.add(Pelicula("The Hunt"))
     }
 
     private val mapPeliculas = mutableMapOf<String, Pelicula>()
