1d3f6b64bbc5b8b6037532c1ee408ab1a27019eb
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2023-10-23 10:16:01 +0200

    23/10/2023 10:15

--- recycledInesMR/app/src/main/java/com/example/recycledinesmr/data/DataMappers.kt
+++ recycledInesMR/app/src/main/java/com/example/recycledinesmr/data/DataMappers.kt
@@ -2,4 +2,4 @@ package com.example.recycledinesmr.data
 
 import com.example.recycledinesmr.domain.modelo.Pelicula
 
-fun Pelicula.toPelicula() : Pelicula = Pelicula(titulo, director, fecha, estrellas, generoComedia, generoTerror, generoRomance, generoTragedia)
+fun Pelicula.toPelicula() : Pelicula = Pelicula(titulo, director, fecha, estrellas, clasificacionEdad)
