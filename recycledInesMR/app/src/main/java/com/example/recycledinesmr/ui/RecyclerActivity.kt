efaece3aba44c9e88fea968bec60168afd0b2b51
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-26 10:59:32 +0200

    26/10/2023 10:59

--- recycledInesMR/app/src/main/java/com/example/recycledinesmr/ui/RecyclerActivity.kt
+++ recycledInesMR/app/src/main/java/com/example/recycledinesmr/ui/RecyclerActivity.kt
@@ -1,4 +1,15 @@
 package com.example.recycledinesmr.ui
 
-class RecyclerActivity {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.example.recycledinesmr.R
+
+class RecyclerActivity : AppCompatActivity() {
+
+    private fun click (titulo:String){
+        Snackbar.make(findViewById<RecyclerView>(R.id.rvPeliculas)
+            , " $titulo", Snackbar.LENGTH_SHORT).show()
+    }
+
 }
