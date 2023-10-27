77a4de4c522c6de6211ea97a3bdde61a10ed8db3
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-10-27 09:14:28 +0200

    27/10/2023 09:14

--- recycledInesMR/app/src/main/java/com/example/recycledinesmr/domain/usecases/GetListaUseCase.kt
+++ recycledInesMR/app/src/main/java/com/example/recycledinesmr/domain/usecases/GetListaUseCase.kt
@@ -1,4 +1,6 @@
 package com.example.recycledinesmr.domain.usecases
 
 class GetListaUseCase {
+
+    //Esto tira de la lista JSON (List<PeliculaJson>)
 }
--- recycledInesMR/app/src/main/java/com/example/recycledinesmr/ui/main/MainActivity.kt
+++ recycledInesMR/app/src/main/java/com/example/recycledinesmr/ui/main/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.recycledinesmr.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.recycledinesmr.R
+import androidx.activity.viewModels
+import com.example.recycledinesmr.data.Repository
 import com.example.recycledinesmr.databinding.ActivityMainBinding
 import com.example.recycledinesmr.domain.usecases.AddPeliculasUseCase
 import com.example.recycledinesmr.domain.usecases.DeletePeliculaUseCase
 import com.example.recycledinesmr.domain.usecases.GetPeliculaUseCase
 import com.example.recycledinesmr.domain.usecases.UpdatePeliculasUseCase
+import com.example.recycledinesmr.ui.Constantes
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by ViewModels{
+    private val viewModel: MainViewModel by viewModels{
         MainViewModelFactory(
             AddPeliculasUseCase(),
             GetPeliculaUseCase(),
@@ -25,6 +30,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(R.layout.activity_main)
+        }
+        observarViewModel()
+        eventos()
+
     }
+    
+    private fun observarViewModel() {
+        viewModel.uiState.observe(this@MainActivity) { state ->
+            state?.let {
+                state.error?.let { error ->
+                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                    viewModel.errorMostrado()
+                }
+                if (state.error == null) {
+                    with(binding) {
+                        retrocederButton.isEnabled = state.indiceActual > 0
+                        avanzarButton.isEnabled =
+                            state.indiceActual < Repository.getPelicula().size - 1
+                        val peli = viewModel.uiState.value?.pelicula
+                        editMovieText.setText(peli?.titulo)
+                        editDirectorText.setText(peli?.director)
+                        editDateText.setText(LocalDate.parse(peli?.fecha.toString()).toString())
+                        when (peli?.clasificacionEdad) {
+                            Constantes.PARA_TODOS -> radioTodos.isChecked = true
+                            Constantes.NO_7 -> radioNo7.isChecked = true
+                            Constantes.NO_12 -> radioNo12.isChecked = true
+                            Constantes.NO_16 -> radioNo16.isChecked = true
+                            Constantes.NO_18 -> radioNo18.isChecked = true
+                        }
+                        estrellasRatingBar.rating = peli?.estrellas?: 0.0f
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun eventos() {
+        TODO("Not yet implemented")
+    }
+
+
 }
