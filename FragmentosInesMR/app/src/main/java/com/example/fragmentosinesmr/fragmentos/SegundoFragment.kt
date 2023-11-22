082aa5fb0367ba22b04a76d2214615a0015e9e4c
cvcvrril <imartinezrodriguez@educa.madrid.org>
2023-11-22 13:50:39 +0100

    22/11/2023 13:50

---
+++ FragmentosInesMR/app/src/main/java/com/example/fragmentosinesmr/fragmentos/SegundoFragment.kt
@@ -0,0 +1,24 @@
+package com.example.fragmentosinesmr.fragmentos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentosinesmr.databinding.FragmentSegundoBinding
+
+class SegundoFragment : Fragment() {
+
+    private var _binding: FragmentSegundoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSegundoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
--- FragmentosInesMR/app/src/main/java/com/example/fragmentosinesmr/main/MainActivity.kt
+++ FragmentosInesMR/app/src/main/java/com/example/fragmentosinesmr/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.fragmentosinesmr.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.fragmentosinesmr.R
 import com.example.fragmentosinesmr.databinding.ActivityMainBinding
 
@@ -16,27 +18,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        initNav()
 
-        with(binding){
+        with(binding) {
             bottomAppBar.setOnMenuItemClickListener { menuItem ->
                 when (menuItem.itemId) {
                     R.id.fragment_primer -> {
-                            navController.navigate(R.id.fragment_primer)
+                        navController.navigate(R.id.fragment_primer)
                         true
                     }
+
                     R.id.fragment_segundo -> {
                         // Handle rotation icon press
                         true
                     }
+
                     R.id.fragment_tercero -> {
                         // Handle dashboard icon press
                         true
                     }
+
                     else -> false
                 }
             }
 
         }
+    }
+
+    private fun initNav(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomAppBar.setupWithNavController(navController)
     }
 
 }
