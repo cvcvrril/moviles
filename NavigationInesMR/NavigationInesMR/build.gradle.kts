71954f9653e09dbae18b85d389bacebd2ba8a12c
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2023-11-27 11:29:34 +0100

    27/11/2023 11:29

---
+++ NavigationInesMR/NavigationInesMR/build.gradle.kts
@@ -0,0 +1,16 @@
+
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+    }
+
+
+}
