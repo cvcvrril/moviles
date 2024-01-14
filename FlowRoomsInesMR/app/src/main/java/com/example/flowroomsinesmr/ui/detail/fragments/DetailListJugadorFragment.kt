1f3f8156c43eaf65951e6a82fc7ae692099f8c12
ines (casa) <imartinezrodriguez@educa.madrid.org>
2024-01-14 10:16:58 +0100

    14/01/2024 10:16

--- FlowRoomsInesMR/app/src/main/java/com/example/flowroomsinesmr/ui/detail/fragments/DetailListJugadorFragment.kt
+++ FlowRoomsInesMR/app/src/main/java/com/example/flowroomsinesmr/ui/detail/fragments/DetailListJugadorFragment.kt
@@ -1,4 +1,27 @@
 package com.example.flowroomsinesmr.ui.detail.fragments
 
-class DetailListJugadorFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flowroomsinesmr.databinding.FragmentDetailJugadorListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailListJugadorFragment : Fragment(){
+
+    private var _binding: FragmentDetailJugadorListBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailJugadorListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
