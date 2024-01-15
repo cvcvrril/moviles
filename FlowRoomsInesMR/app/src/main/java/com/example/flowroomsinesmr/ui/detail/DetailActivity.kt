96326f0dcb1f55d18e27f30e8e6a3ad482d7797a
ines (casa) <imartinezrodriguez@educa.madrid.org>
2024-01-15 16:29:01 +0100

    15/01/2024 16:28

--- FlowRoomsInesMR/app/src/main/java/com/example/flowroomsinesmr/ui/detail/DetailActivity.kt
+++ FlowRoomsInesMR/app/src/main/java/com/example/flowroomsinesmr/ui/detail/DetailActivity.kt
@@ -2,7 +2,10 @@ package com.example.flowroomsinesmr.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.flowroomsinesmr.databinding.ActivityDetailBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.flowroomsinesmr.R
 import com.example.flowroomsinesmr.databinding.ActivityDetailPruebaBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,11 +13,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class DetailActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityDetailPruebaBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailPruebaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with(binding){
+            setSupportActionBar(topAppBar)
+            drawerLayout.open()
+        }
+        initNav()
+    }
+
+    private fun initNav(){
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.navView.setupWithNavController(navController)
     }
 
 }
